f64814ff01ab71ef32baa65ea197839a21fb04fe
ryohey <info@codingcafe.jp>
2020-05-30 12:27:50 +0900

    update

--- SwiftUI-SceneKit/ContentView.swift
+++ SwiftUI-SceneKit/ContentView.swift
@@ -18,29 +18,36 @@ struct Node: NodeProtocol {
         self.scnNode = scnNode
     }
 
+    init(childNodes: [SCNNode]) {
+        scnNode = SCNNode()
+        childNodes.forEach(scnNode.addChildNode)
+    }
+
     init(configure: ((SCNNode) -> Void)? = nil) {
         let node = SCNNode()
         configure?(node)
         scnNode = node
     }
+}
 
+extension NodeProtocol {
     func geometry(_ geometry: SCNGeometry?) -> Self {
-        scnNode.geometry = geometry
+        getNode().scnNode.geometry = geometry
         return self
     }
 
     func light(_ light: SCNLight?) -> Self {
-        scnNode.light = light
+        getNode().scnNode.light = light
         return self
     }
 
     func camera(_ camera: SCNCamera?) -> Self {
-        scnNode.camera = camera
+        getNode().scnNode.camera = camera
         return self
     }
 
     func position(_ position: SCNVector3) -> Self {
-        scnNode.position = position
+        getNode().scnNode.position = position
         return self
     }
 }
@@ -49,20 +56,69 @@ extension Never: NodeProtocol {
 
 }
 
-@_functionBuilder struct SCNNodeBuilder {
-    static func buildBlock<Content>(_ content: Content) -> Content where Content : SCNNode {
-        content
-    }
-}
-
 @_functionBuilder struct NodeBuilder {
     static func buildBlock(_ content: Node) -> Node {
         content
     }
 
-    static func buildBlock(_ contents: Node...) -> Node {
+    static func buildBlock<C0>(_ c0: C0) -> Node where C0: NodeProtocol {
         return Node { node in
-            contents.forEach { node.addChildNode($0.scnNode) }
+            node.addChildNode(c0.getNode().scnNode)
+        }
+    }
+
+    static func buildBlock<C0, C1>(
+        _ c0: C0,
+        _ c1: C1
+    ) -> Node where
+        C0: NodeProtocol,
+        C1: NodeProtocol {
+            return Node(childNodes: [
+                c0.getNode().scnNode,
+                c1.getNode().scnNode,
+            ])
+    }
+
+    static func buildBlock<C0, C1, C2>(
+        _ c0: C0,
+        _ c1: C1,
+        _ c2: C2
+    ) -> Node where
+        C0: NodeProtocol,
+        C1: NodeProtocol,
+        C2: NodeProtocol {
+            return Node(childNodes: [
+                c0.getNode().scnNode,
+                c1.getNode().scnNode,
+                c2.getNode().scnNode,
+            ])
+    }
+
+    static func buildBlock<C0, C1, C2, C3>(
+        _ c0: C0,
+        _ c1: C1,
+        _ c2: C2,
+        _ c3: C3
+    ) -> Node where
+        C0: NodeProtocol,
+        C1: NodeProtocol,
+        C2: NodeProtocol,
+        C3: NodeProtocol {
+            return Node(childNodes: [
+                c0.getNode().scnNode,
+                c1.getNode().scnNode,
+                c2.getNode().scnNode,
+                c3.getNode().scnNode,
+            ])
+    }
+}
+
+extension NodeProtocol {
+    func getNode() -> Node {
+        if let node = self as? Node {
+            return node
+        } else {
+            return body.getNode()
         }
     }
 }
@@ -112,13 +168,35 @@ protocol NodeProtocol {
     var body: Self.Body { get }
 }
 
-struct AmbientLight: NodeProtocol {
-    let light = SCNLight()
+struct Camera: NodeProtocol {
+    var body: some NodeProtocol {
+        Node()
+            .camera(SCNCamera())
+            .position(SCNVector3(x:0, y:0, z:5))
+    }
+}
 
+struct OmniLight: NodeProtocol {
+    var body: some NodeProtocol {
+        Node()
+            .light(light)
+            .position(SCNVector3(x:0, y:10, z:10))
+    }
+
+    private let light = SCNLight()
+
+    init() {
+        light.type = .omni
+    }
+}
+
+struct AmbientLight: NodeProtocol {
     var body: some NodeProtocol {
         Node()
             .light(light)
     }
+
+    private let light = SCNLight()
 
     init() {
         light.type = .ambient
@@ -126,51 +204,50 @@ struct AmbientLight: NodeProtocol {
     }
 }
 
+struct Box: NodeProtocol {
+    var body: some NodeProtocol {
+        Node { $0.addAnimation(self.animation, forKey: nil) }
+            .geometry(geometry)
+    }
+
+    private let animation = CABasicAnimation(keyPath: (\SCNNode.rotation).string)
+    private let geometry = SCNBox(width:1, height:1, length:1, chamferRadius:0.02)
+
+    init() {
+        // create and configure a material
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.green
+        material.specular.contents = UIColor.gray
+        material.locksAmbientWithDiffuse = true
+
+        // set the material to the 3D object geometry
+        geometry.firstMaterial = material
+
+        animation.toValue = NSValue(scnVector4:SCNVector4(x:1, y:1, z:0, w:Float.pi * 2))
+        animation.duration = 5
+        animation.repeatCount = MAXFLOAT // repeat forever
+    }
+}
+
+extension KeyPath where Root: NSObject {
+    var string: String {
+        NSExpression(forKeyPath: self).keyPath
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         SceneView {
             Scene {
-                Node()
-                    .camera(SCNCamera())
+                Camera()
                     .position(SCNVector3(x:0, y:0, z:5))
-
-                Node()
-                    .light({
-                        let light = SCNLight()
-                        light.type = .omni
-                        return light
-                        }())
-                    .position(SCNVector3(x:0, y:10, z:10))
-
-
-                // create and add a 3D box to the scene
-                Node { node in
-                    // animate the 3D object
-                    let animation:CABasicAnimation = CABasicAnimation(keyPath:"rotation")
-                    animation.toValue = NSValue(scnVector4:SCNVector4(x:1, y:1, z:0, w:Float.pi * 2))
-                    animation.duration = 5
-                    animation.repeatCount = MAXFLOAT // repeat forever
-                    node.addAnimation(animation, forKey:nil)
-                }
-                .geometry({
-                    let geometry = SCNBox(width:1, height:1, length:1, chamferRadius:0.02)
-
-                    // create and configure a material
-                    let material = SCNMaterial()
-                    material.diffuse.contents = UIColor.green
-                    material.specular.contents = UIColor.gray
-                    material.locksAmbientWithDiffuse = true
-
-                    // set the material to the 3D object geometry
-                    geometry.firstMaterial = material
-
-                    return geometry
-                    }())
+                OmniLight()
+                AmbientLight()
+                Box()
             }
         }
     }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
