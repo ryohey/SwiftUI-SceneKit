795c6f055698bc229c6b5bf12b995a4992c6ba80
ryohey <info@codingcafe.jp>
2020-05-30 09:44:15 +0900

    Initial Commit

---
+++ SwiftUI-SceneKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-SceneKit
+//
+//  Created by ryohey on 2020/05/30.
+//  Copyright © 2020 ryohey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
